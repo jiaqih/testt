deb111e9010492cd5e1659b03b2a17e14ee81741
jiaqih <huangjiaqi811@live.com>
2015-10-29 00:19:08 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  gittest
+//
+//  Created by Huang Jiaqi on 15/10/29.
+//  Copyright © 2015年 Jiaqi Huang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
